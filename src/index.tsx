9abee21354473e4a85ba2534acec6ae700695d31
Aman Yadav <amanpdyadav@gmail.com>
2021-04-15 11:28:42 +0300

    Update styles to use open city design
    * Removed the index.scss instead use main.scss from
      styles folder.
    * Import open city design to mains.scss
    
    Refs PV-46

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.scss';
+import './i18n/i18n';
+import './styles/main.scss';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
