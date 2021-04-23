ee243a692491d7822e389ca3bc2a04b39893c0ac
Aman Yadav <amanpdyadav@gmail.com>
2021-04-23 09:51:17 +0300

    Add configuration for OIDC client
    Refs PV-84

---
+++ src/client/config.ts
@@ -0,0 +1,43 @@
+import { ClientProps } from './types';
+
+function envValueToBoolean(
+  value: string | undefined | boolean,
+  defaultValue: boolean
+): boolean {
+  const strValue = String(value).toLowerCase();
+  if (
+    value === false ||
+    strValue === '' ||
+    strValue === 'false' ||
+    strValue === '0'
+  ) {
+    return false;
+  }
+  if (value === true || strValue === 'true' || strValue === '1') {
+    return true;
+  }
+  return defaultValue;
+}
+
+const clientConfig: ClientProps = {
+  realm: String(process.env.REACT_APP_OIDC_REALM),
+  url: String(process.env.REACT_APP_OIDC_URL),
+  authority: process.env.REACT_APP_OIDC_REALM
+    ? `${process.env.REACT_APP_OIDC_URL}/realms/${process.env.REACT_APP_OIDC_REALM}`
+    : String(process.env.REACT_APP_OIDC_URL),
+  clientId: String(process.env.REACT_APP_OIDC_CLIENT_ID),
+  callbackPath: String(process.env.REACT_APP_OIDC_CALLBACK_PATH),
+  logoutPath: process.env.REACT_APP_OIDC_LOGOUT_PATH || '/',
+  silentAuthPath: process.env.REACT_APP_OIDC_SILENT_AUTH_PATH,
+  responseType: process.env.REACT_APP_OIDC_RESPONSE_TYPE,
+  scope: process.env.REACT_APP_OIDC_SCOPE,
+  autoSignIn: envValueToBoolean(process.env.REACT_APP_OIDC_AUTO_SIGN_IN, true),
+  automaticSilentRenew: envValueToBoolean(
+    process.env.REACT_APP_OIDC_AUTO_SILENT_RENEW,
+    true
+  ),
+  enableLogging: envValueToBoolean(process.env.REACT_APP_OIDC_LOGGING, false),
+  tokenExchangePath: process.env.REACT_APP_OIDC_TOKEN_EXCHANGE_PATH,
+};
+
+export default clientConfig;
