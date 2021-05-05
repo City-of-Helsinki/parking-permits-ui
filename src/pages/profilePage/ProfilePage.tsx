fe3f7089cd931b7fb33c9168575998222f2b454f
Aman Yadav <amanpdyadav@gmail.com>
2021-05-05 11:18:08 +0300

    Update user profile page to render name and address
    Refs PV-79

--- src/pages/profilePage/ProfilePage.tsx
+++ src/pages/profilePage/ProfilePage.tsx
@@ -1,88 +1,57 @@
 import React, { useContext } from 'react';
-import { Button } from 'hds-react';
-import { ApiAccessTokenActions } from '../../client/hooks';
-import { ProfileDataType, useProfile } from '../../profile';
-import { AnyObject } from '../../common/types';
+import { useSelector } from 'react-redux';
+import { Card, Container } from 'reactstrap';
+import { ApiAccessTokenActions } from '../../client/types';
 import { ApiAccessTokenContext } from '../../common/ApiAccessTokenProvider';
-
-const nodeToJSON = (node: AnyObject): AnyObject | AnyObject[] => {
-  if (Array.isArray(node.edges)) {
-    return node.edges.map(edge => nodeToJSON(edge.node) as AnyObject);
-  }
-  if (node.__typename === 'VerifiedPersonalInformationNode') {
-    return node;
-  }
-  return {
-    id: String(node.id),
-    value: String(
-      node.address
-        ? `${node.address} ${node.postalCode} ${node.city} ${node.countryCode}`
-        : node.email || node.phone
-    ),
-    primary: String(node.primary),
-  };
-};
-
-const PropToComponent = ([prop, value]: [
-  string,
-  ProfileDataType
-]): React.ReactElement => (
-  <li key={prop}>
-    <strong>{prop}</strong>:{' '}
-    {value && typeof value === 'object' ? (
-      <pre data-test-id={`profile-data-${prop}`}>
-        {JSON.stringify(nodeToJSON(value), null, 2)}
-      </pre>
-    ) : (
-      <span data-test-id={`profile-data-${prop}`}>{value || '-'}</span>
-    )}
-  </li>
-);
+import { StoreState, UserAddress } from '../../redux';
 
 const ProfilePage = (): React.ReactElement => {
   const actions = useContext(ApiAccessTokenContext) as ApiAccessTokenActions;
+  const { helsinkiProfileState } = useSelector((state: StoreState) => state);
+  const { firstName, lastName, addresses } = helsinkiProfileState.profile;
   const { getStatus: getApiAccessTokenStatus } = actions;
-  const {
-    getStatus: getProfileStatus,
-    getProfile,
-    fetch,
-    clear,
-    getErrorMessage,
-    getResultErrorMessage,
-  } = useProfile();
   const apiAccessTokenStatus = getApiAccessTokenStatus();
-  const profileStatus = getProfileStatus();
-  const profileData = getProfile();
-  const resultErrorMessage = getResultErrorMessage();
-  const reload = async (): Promise<void> => {
-    await clear();
-    await fetch();
-  };
   if (apiAccessTokenStatus === 'error') {
     return <div>Api access tokenin lataus epäonnistui</div>;
   }
-  if (profileStatus === 'error') {
-    return (
-      <div data-test-id="profile-load-error">
-        Profiilin lataus epäonnistui:
-        <pre>{getErrorMessage()}</pre>
+
+  const getAddresses = (userAddresses: UserAddress[]) =>
+    userAddresses.map(address => (
+      <div key={address.id}>
+        <span
+          style={{
+            fontSize: 'var(--fontsize-heading-s)',
+            marginTop: 'var(--spacing-s)',
+          }}>
+          {address.primary ? 'Primary Address: ' : 'Other address: '}
+        </span>
+        <span>{`${address.address}, ${address.postalCode}, ${address.city}`}</span>
       </div>
-    );
-  }
-  if (profileStatus !== 'loaded') {
-    return <div>Ladataan....</div>;
-  }
+    ));
   return (
-    <div>
-      <h2>Profiilin tiedot:</h2>
-      {profileData && (
-        <ul>{Object.entries(profileData).map(arr => PropToComponent(arr))}</ul>
-      )}
-      {resultErrorMessage && (
-        <p data-test-id="profile-data-result-error">{resultErrorMessage}</p>
-      )}
-      <Button onClick={reload}>Hae</Button>
-    </div>
+    <Container>
+      <div style={{ fontSize: 'var(--fontsize-heading-m)' }}>
+        Helsinki profile:
+      </div>
+      <Card
+        style={{ marginTop: 'var(--spacing-s)', padding: 'var(--spacing-xs)' }}>
+        <div>
+          <span
+            style={{
+              fontSize: 'var(--fontsize-heading-s)',
+              marginTop: 'var(----spacing-s)',
+            }}>
+            Name:{' '}
+          </span>
+          <span>{`${firstName} ${lastName}`}</span>
+        </div>
+        {getAddresses(Object.values(addresses))}
+      </Card>
+      <div style={{ fontSize: 'var(--fontsize-heading-m)' }}>Data:</div>
+      <Card>
+        <pre>{JSON.stringify(helsinkiProfileState.profile, null, 2)}</pre>
+      </Card>
+    </Container>
   );
 };
 
