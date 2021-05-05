7f08b13ebf239fe3f85ca28317e04361ca150c11
Aman Yadav <amanpdyadav@gmail.com>
2021-05-05 11:16:15 +0300

    Remove unnecessary types
    Refs PV-79

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,5 +1,3 @@
-import { Feature, MultiPolygon } from 'geojson';
-
 export type AnyObject = Record<string, unknown>;
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type AnyNonNullishValue = {};
@@ -7,7 +5,3 @@ export type AnyValue = AnyNonNullishValue | undefined | null;
 export type AnyFunction = (props?: unknown) => unknown;
 
 export type Point = [number, number];
-
-export interface Region extends Feature<MultiPolygon> {
-  id: string;
-}
