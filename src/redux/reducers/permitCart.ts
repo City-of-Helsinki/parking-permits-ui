5526351667fb0aec65256f8b763caa021b2e0297
Aman Yadav <amanpdyadav@gmail.com>
2021-05-11 10:05:08 +0300

    Add reducers for set address id action
    Refs PV-94

--- src/redux/reducers/permitCart.ts
+++ src/redux/reducers/permitCart.ts
@@ -1,18 +1,23 @@
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
 
 import { PermitCartState } from '../types';
-import { setCurrentStepperAction } from '../actions/permitCart';
+import {
+  setCurrentStepperAction,
+  setSelectedAddressIdAction,
+} from '../actions/permitCart';
 
 const initialState: PermitCartState = {
   currentStep: 1,
 };
 
-const cartReducer = reducerWithInitialState<PermitCartState>(initialState).case(
-  setCurrentStepperAction,
-  (state, action) => ({
+const cartReducer = reducerWithInitialState<PermitCartState>(initialState)
+  .case(setCurrentStepperAction, (state, action) => ({
     ...state,
     currentStep: action,
-  })
-);
+  }))
+  .case(setSelectedAddressIdAction, (state, action) => ({
+    ...state,
+    selectedAddressId: action,
+  }));
 
 export default cartReducer;
