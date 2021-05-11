6aa195e69f183f35d915b6ed18a49a71d158898a
Aman Yadav <amanpdyadav@gmail.com>
2021-05-11 10:09:20 +0300

    Replace addressSelector part with its own component
    Refs PV-94

--- src/pages/frontPage/FrontPage.tsx
+++ src/pages/frontPage/FrontPage.tsx
@@ -2,35 +2,37 @@ import React, { useContext, useLayoutEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { Container } from 'reactstrap';
-import { Button, IconArrowRight, Notification } from 'hds-react';
 
 import Hero from '../../common/hero/Hero';
 import { ApiAccessTokenContext } from '../../common/apiAccessTokenProvider';
 import { ApiAccessTokenActions } from '../../client/types';
 import { StoreState } from '../../redux';
 import { fetchUserProfile } from '../../redux/actions/helsinkiProfile';
-import ParkingZonesMap from '../../common/parkingZoneMap/ParkingZonesMap';
-import Address from '../../common/address/Address';
 
 import './frontPage.scss';
 import Stepper from '../../common/stepper/Stepper';
-import { setCurrentStepper } from '../../redux/actions/permitCart';
+import AddressSelector from '../../common/addressSelector/AddressSelector';
+import { setSelectedAddressId } from '../../redux/actions/permitCart';
 
 const FrontPage = (): React.ReactElement => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
   useLayoutEffect(() => window.scrollTo(0, 0), []);
-  const [selectedItem, setSelectedItem] = React.useState('');
   const { featuresState, helsinkiProfileState, permitCartState } = useSelector(
     (state: StoreState) => state
   );
 
-  const { currentStep } = permitCartState;
   const addresses = Object.values(
-    helsinkiProfileState?.profile?.addresses || {}
+    helsinkiProfileState.profile?.addresses || {}
   );
+
+  const { currentStep, selectedAddressId } = permitCartState;
   const actions = useContext(ApiAccessTokenContext) as ApiAccessTokenActions;
   const { getStatus: getApiAccessTokenStatus } = actions;
+
+  if (addresses.length && !selectedAddressId?.length) {
+    dispatch(setSelectedAddressId(addresses[0].id));
+  }
 
   if (
     getApiAccessTokenStatus() === 'loaded' &&
@@ -38,18 +40,6 @@ const FrontPage = (): React.ReactElement => {
   ) {
     dispatch(fetchUserProfile());
   }
-
-  if (addresses.length && !selectedItem?.length) {
-    setSelectedItem(addresses[0].id);
-  }
-
-  const onChange = (event: { target: { value: string } }) => {
-    setSelectedItem(event.target.value);
-  };
-
-  const gotoNext = () => {
-    dispatch(setCurrentStepper(currentStep + 1));
-  };
   return (
     <div className="front-page">
       <Hero
@@ -64,35 +54,13 @@ const FrontPage = (): React.ReactElement => {
           className="only-for-mobile"
           style={{ marginBottom: 'var(--spacing-s)' }}
         />
-        <Notification
-          className="notification"
-          label={t('page.frontPage.notification.info.label')}>
-          {t('page.frontPage.notification.info.message')}
-        </Notification>
-        <div className="select-address-title">
-          {t('page.frontPage.selectAddress')}
-        </div>
-        <div className="addresses">
-          {addresses?.length &&
-            addresses.map(address => (
-              <Address
-                key={address.id}
-                selectedItem={selectedItem}
-                address={address}
-                onChange={onChange}>
-                {featuresState.features[address.id] && (
-                  <ParkingZonesMap
-                    featureCollection={featuresState.features[address.id]}
-                    zoom={13}
-                  />
-                )}
-              </Address>
-            ))}
-        </div>
-        <Button className="action-btn" onClick={gotoNext}>
-          <span>{t('page.frontPage.buyParkingId')}</span>
-          <IconArrowRight />
-        </Button>
+        {selectedAddressId && (
+          <AddressSelector
+            selectedAddressId={selectedAddressId}
+            addresses={addresses}
+            features={featuresState.features}
+          />
+        )}
       </Container>
     </div>
   );
