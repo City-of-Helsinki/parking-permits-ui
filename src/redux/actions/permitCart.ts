177fca83c118ee69b19e00514c8442fca26d58b8
Aman Yadav <amanpdyadav@gmail.com>
2021-05-11 10:04:28 +0300

    Add action for updating address id
    Refs PV-94

--- src/redux/actions/permitCart.ts
+++ src/redux/actions/permitCart.ts
@@ -7,6 +7,15 @@ const actionCreator = actionCreatorFactory('permitCart');
 export const setCurrentStepperAction = actionCreator<number>(
   'set-current-stepper'
 );
+export const setSelectedAddressIdAction = actionCreator<string>(
+  'set-selected-address-id'
+);
 export const setCurrentStepper = (id: number) => (dispatch: Dispatch): void => {
   dispatch(setCurrentStepperAction(id));
+};
+
+export const setSelectedAddressId = (id: string) => (
+  dispatch: Dispatch
+): void => {
+  dispatch(setSelectedAddressIdAction(id));
 };
