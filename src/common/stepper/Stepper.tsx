588a378d13ac7175c0f2f22c7d3fded064e349cb
Aman Yadav <amanpdyadav@gmail.com>
2021-05-19 09:07:37 +0300

    Fix stepper for selected and completed state
    Refs PV-117

--- src/common/stepper/Stepper.tsx
+++ src/common/stepper/Stepper.tsx
@@ -23,23 +23,24 @@ const Stepper: FC<Props> = ({
       })}
       style={{ ...style }}>
       {Array.from({ length: stepLength })
-        .map((v, i) => i)
+        .map((v, i) => i + 1)
         .map(value => (
           <div
             key={uuidv4()}
             className={classNames('steps', {
-              last: value + 1 === stepLength,
+              last: value === stepLength,
             })}>
             <div
               className={classNames('step', {
+                selected: value === currentStep,
                 completed: value < currentStep,
               })}>
-              {value + 1}
+              {value}
             </div>
-            {value + 1 !== stepLength && (
+            {value !== stepLength && (
               <div
                 className={classNames('step-length', {
-                  completed: value + 1 < currentStep,
+                  completed: value < currentStep,
                 })}
               />
             )}
