02c17334add58d1aac85c96c327bbd4961b5fa35
Aman Yadav <amanpdyadav@gmail.com>
2021-05-28 10:17:14 +0300

    Update front page to show purchased permit
    Refs PV-120

--- src/pages/frontPage/FrontPage.tsx
+++ src/pages/frontPage/FrontPage.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useLayoutEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { Button, IconPen, IconTrash } from 'hds-react';
 import { Container } from 'reactstrap';
 
 import Hero from '../../common/hero/Hero';
@@ -12,10 +13,15 @@ import { fetchUserProfile } from '../../redux/actions/helsinkiProfile';
 import './frontPage.scss';
 import Stepper from '../../common/stepper/Stepper';
 import AddressSelector from '../../common/addressSelector/AddressSelector';
-import { setSelectedAddressId } from '../../redux/actions/permitCart';
+import {
+  setPurchased,
+  setSelectedAddressId,
+  fetchVehicleDetail,
+} from '../../redux/actions/permitCart';
 import VehicleSelector from '../../common/vehicleSelector/VehicleSelector';
 import ShoppingCart from '../../common/shoppingCart/ShoppingCart';
 import PurchasedOverview from '../../common/purchasedOverview/PurchasedOverview';
+import Permit from '../../common/permit/Permit';
 
 const FrontPage = (): React.ReactElement => {
   const { t } = useTranslation();
@@ -34,12 +40,18 @@ const FrontPage = (): React.ReactElement => {
     selectedAddressId,
     vehicleDetail,
     prices,
+    purchased,
+    validityPeriod,
     parkingDurationType,
     parkingStartType,
   } = permitCartState;
   const actions = useContext(ApiAccessTokenContext) as ApiAccessTokenActions;
   const { getStatus: getApiAccessTokenStatus } = actions;
 
+  const editPermit = () => {
+    dispatch(setPurchased(false));
+    dispatch(fetchVehicleDetail(undefined));
+  };
   if (addresses.length && !selectedAddressId?.length) {
     dispatch(setSelectedAddressId(addresses[0].id));
   }
@@ -50,6 +62,58 @@ const FrontPage = (): React.ReactElement => {
   ) {
     dispatch(fetchUserProfile());
   }
+  const getStepperComponent = (step: number) => {
+    const component = <div />;
+    if (!selectedAddressId) return component;
+    if (step === 1 && purchased && vehicleDetail)
+      return (
+        <Permit
+          address={helsinkiProfileState.profile.addresses[selectedAddressId]}
+          vehicleDetail={vehicleDetail}
+          validityPeriod={validityPeriod}
+        />
+      );
+
+    if (step === 1) {
+      return (
+        <AddressSelector
+          selectedAddressId={selectedAddressId}
+          addresses={addresses}
+          features={featuresState.features}
+        />
+      );
+    }
+    if (step === 2) {
+      return (
+        <VehicleSelector
+          prices={prices}
+          vehicleDetail={vehicleDetail}
+          address={helsinkiProfileState.profile.addresses[selectedAddressId]}
+        />
+      );
+    }
+    if (vehicleDetail) {
+      return step === 3 ? (
+        <ShoppingCart
+          prices={prices}
+          parkingDurationType={parkingDurationType}
+          parkingStartType={parkingStartType}
+          vehicleDetail={vehicleDetail}
+          address={helsinkiProfileState.profile.addresses[selectedAddressId]}
+        />
+      ) : (
+        <PurchasedOverview
+          prices={prices}
+          validityPeriod={validityPeriod}
+          parkingDurationType={parkingDurationType}
+          parkingStartType={parkingStartType}
+          vehicleDetail={vehicleDetail}
+          address={helsinkiProfileState.profile.addresses[selectedAddressId]}
+        />
+      );
+    }
+    return component;
+  };
   return (
     <div className="front-page">
       <Hero
@@ -60,37 +124,19 @@ const FrontPage = (): React.ReactElement => {
       />
       <Container>
         <Stepper currentStep={currentStep} className="only-for-mobile" />
-        {selectedAddressId && currentStep === 1 && (
-          <AddressSelector
-            selectedAddressId={selectedAddressId}
-            addresses={addresses}
-            features={featuresState.features}
-          />
-        )}
-        {selectedAddressId && currentStep === 2 && (
-          <VehicleSelector
-            prices={prices}
-            vehicleDetail={vehicleDetail}
-            address={helsinkiProfileState.profile.addresses[selectedAddressId]}
-          />
-        )}
-        {selectedAddressId && currentStep === 3 && vehicleDetail && (
-          <ShoppingCart
-            prices={prices}
-            parkingDurationType={parkingDurationType}
-            parkingStartType={parkingStartType}
-            vehicleDetail={vehicleDetail}
-            address={helsinkiProfileState.profile.addresses[selectedAddressId]}
-          />
-        )}
-        {selectedAddressId && currentStep === 4 && vehicleDetail && (
-          <PurchasedOverview
-            prices={prices}
-            parkingDurationType={parkingDurationType}
-            parkingStartType={parkingStartType}
-            vehicleDetail={vehicleDetail}
-            address={helsinkiProfileState.profile.addresses[selectedAddressId]}
-          />
+        {getStepperComponent(currentStep)}
+        {currentStep === 1 && purchased && (
+          <div className="action-buttons">
+            <Button className="action-btn" onClick={editPermit}>
+              <span>{t('page.frontPage.editPermit')}</span>
+              <IconPen />
+            </Button>
+
+            <Button className="action-btn" variant="danger">
+              <span>{t('page.frontPage.deletePermit')}</span>
+              <IconTrash />
+            </Button>
+          </div>
         )}
       </Container>
     </div>
