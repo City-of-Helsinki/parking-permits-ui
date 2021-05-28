9ce6dadcecd106e124e270d884b458033e4c0938
Aman Yadav <amanpdyadav@gmail.com>
2021-05-28 10:13:24 +0300

    Add swedish language
    Refs PV-120

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from 'react-i18next';
 
 import fi from './fi.json';
 import en from './en.json';
+import sv from './sv.json';
 
 i18n.use(initReactI18next).init({
   fallbackLng: 'fi',
@@ -13,6 +14,9 @@ i18n.use(initReactI18next).init({
   resources: {
     fi: {
       translation: fi,
+    },
+    sv: {
+      translation: sv,
     },
     en: {
       translation: en,
