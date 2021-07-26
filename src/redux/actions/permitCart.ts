22a3f637931db34d74e0457c54a6130f526733ed
Aman Yadav <amanpdyadav@gmail.com>
2021-07-26 12:15:50 +0300

    Move test data to separate file
    Test data will be removed once the backend
    is ready. And there for its easy to put it
    to a separate file for future removal.
    
    Refs PV-196

--- src/redux/actions/permitCart.ts
+++ src/redux/actions/permitCart.ts
@@ -1,10 +1,16 @@
+import { ThunkDispatch } from 'redux-thunk';
+import { AnyAction, Dispatch } from 'redux';
 import actionCreatorFactory from 'typescript-fsa';
-import { Dispatch } from 'redux';
+
 import {
   ParkingDurationType,
   ParkingStartType,
+  Permit,
+  STEPPER,
+  UserAddress,
   ValidityPeriod,
 } from '../types';
+import { getPermit } from './testHTTPResponse';
 
 const actionCreator = actionCreatorFactory('permitCart');
 
@@ -12,76 +18,152 @@ const actionCreator = actionCreatorFactory('permitCart');
 export const setCurrentStepperAction = actionCreator<number>(
   'set-current-stepper'
 );
-export const setSelectedAddressIdAction = actionCreator<string>(
-  'set-selected-address-id'
+
+export const setSelectedAddressAction = actionCreator<UserAddress>(
+  'set-selected-address'
 );
-export const setParkingStartTypeAction = actionCreator<ParkingStartType>(
-  'parking-start-type'
+export const setParkingStartTypeAction = actionCreator<{
+  registration: string;
+  type: ParkingStartType;
+}>('parking-start-type');
+
+export const setParkingStartDateAction = actionCreator<{
+  registration: string;
+  date: Date;
+}>('parking-start-date');
+
+export const setParkingDurationTypeAction = actionCreator<{
+  registration: string;
+  type: ParkingDurationType;
+}>('parking-duration-type');
+
+export const setParkingDurationPeriodAction = actionCreator<{
+  registration: string;
+  duration: number;
+}>('parking-duration-period');
+
+export const addRegistrationAction = actionCreator<string>(
+  'add-registration-action'
 );
-export const setParkingStartDateAction = actionCreator<Date>(
-  'parking-start-date'
+
+export const deleteRegistrationAction = actionCreator<string>(
+  'delete-registration-action'
 );
-export const setParkingDurationTypeAction = actionCreator<ParkingDurationType>(
-  'parking-duration-type'
-);
-export const fetchVehicleDetailAction = actionCreator<string | undefined>(
-  'fetch-vehicle-detail'
-);
-export const setParkingDurationPeriodAction = actionCreator<number>(
-  'parking-duration-period'
-);
-export const setValidityPeriodAction = actionCreator<ValidityPeriod>(
-  'validity-period'
-);
-export const setPurchasedAction = actionCreator<boolean>('is-purchased');
-export const setCurrentStepper = (id: number) => (dispatch: Dispatch): void => {
-  dispatch(setCurrentStepperAction(id));
-};
 
-export const setParkingStartType = (type: ParkingStartType) => (
-  dispatch: Dispatch
-): void => {
-  dispatch(setParkingStartTypeAction(type));
-};
+export const updateRegistrationAction = actionCreator<{
+  registration: string;
+  index: number;
+}>('set-registration-action');
 
-export const setParkingStartDate = (date: Date) => (
-  dispatch: Dispatch
-): void => {
-  dispatch(setParkingStartDateAction(date));
-};
+export const setValidityPeriodAction = actionCreator<{
+  registration: string;
+  period: ValidityPeriod;
+}>('validity-period');
 
-export const setParkingDurationType = (type: ParkingDurationType) => (
-  dispatch: Dispatch
-): void => {
-  dispatch(setParkingDurationTypeAction(type));
-};
+export const setPrimaryVehicleAction = actionCreator<{
+  registration: string;
+  primary: boolean;
+}>('set-primary-vehicle');
 
-export const setParkingDurationPeriod = (duration: number) => (
-  dispatch: Dispatch
-): void => {
-  dispatch(setParkingDurationPeriodAction(duration));
-};
+export const fetchVehicleAndPermitPricesAction = actionCreator.async<
+  Record<string, unknown>,
+  { [reg: string]: Permit },
+  Error
+>('fetch');
 
-export const setSelectedAddressId = (id: string) => (
-  dispatch: Dispatch
-): void => {
-  dispatch(setSelectedAddressIdAction(id));
-};
+export const setCurrentStepper =
+  (id: number) =>
+  (dispatch: Dispatch): void => {
+    dispatch(setCurrentStepperAction(id));
+  };
 
-export const fetchVehicleDetail = (reg: string | undefined) => (
-  dispatch: Dispatch
-): void => {
-  dispatch(fetchVehicleDetailAction(reg));
-};
+export const setParkingStartType =
+  (registration: string, type: ParkingStartType) =>
+  (dispatch: Dispatch): void => {
+    dispatch(setParkingStartTypeAction({ registration, type }));
+  };
 
-export const setValidityPeriod = (period: ValidityPeriod) => (
-  dispatch: Dispatch
-): void => {
-  dispatch(setValidityPeriodAction(period));
-};
+export const setParkingStartDate =
+  (registration: string, date: Date) =>
+  (dispatch: Dispatch): void => {
+    dispatch(setParkingStartDateAction({ registration, date }));
+  };
 
-export const setPurchased = (purchased: boolean) => (
-  dispatch: Dispatch
-): void => {
-  dispatch(setPurchasedAction(purchased));
-};
+export const setParkingDurationType =
+  (registration: string, type: ParkingDurationType) =>
+  (dispatch: Dispatch): void => {
+    dispatch(setParkingDurationTypeAction({ registration, type }));
+  };
+
+export const setParkingDurationPeriod =
+  (registration: string, duration: number) =>
+  (dispatch: Dispatch): void => {
+    dispatch(setParkingDurationPeriodAction({ registration, duration }));
+  };
+
+export const setValidityPeriod =
+  (registration: string, period: ValidityPeriod) =>
+  (dispatch: Dispatch): void => {
+    dispatch(setValidityPeriodAction({ registration, period }));
+  };
+
+export const setSelectedAddress =
+  (address: UserAddress) =>
+  (dispatch: Dispatch): void => {
+    dispatch(setSelectedAddressAction(address));
+  };
+
+export const addRegistration =
+  (registrationNumber: string) =>
+  (dispatch: Dispatch): void => {
+    dispatch(addRegistrationAction(registrationNumber));
+  };
+
+export const setRegistration =
+  (registrationNumber: string, index: number) =>
+  (dispatch: Dispatch): void => {
+    dispatch(
+      updateRegistrationAction({
+        registration: registrationNumber,
+        index,
+      })
+    );
+  };
+
+export const deleteRegistration =
+  (registration: string) =>
+  (dispatch: Dispatch): void => {
+    dispatch(deleteRegistrationAction(registration));
+  };
+
+export const setPrimaryVehicle =
+  (registration: string, primary: boolean) =>
+  (dispatch: Dispatch): void => {
+    dispatch(setPrimaryVehicleAction({ registration, primary }));
+  };
+
+export const fetchVehicleAndPrices =
+  (registrationNumbers: string[]) =>
+  async (
+    dispatch: ThunkDispatch<
+      Record<string, unknown>,
+      Record<string, unknown>,
+      AnyAction
+    >
+  ): Promise<void> => {
+    dispatch(fetchVehicleAndPermitPricesAction.started({}));
+    // eslint-disable-next-line no-magic-numbers
+    await new Promise(res => setTimeout(res, 500));
+    const result: { [reg: string]: Permit } = {};
+    registrationNumbers.forEach((reg, i) => {
+      result[reg] = getPermit(reg, i === 0);
+    });
+
+    dispatch(
+      fetchVehicleAndPermitPricesAction.done({
+        params: {},
+        result,
+      })
+    );
+    dispatch(setCurrentStepper(STEPPER.PERMIT_PRICES));
+  };
--- src/redux/reducers/helsinkiProfile.ts
+++ src/redux/reducers/helsinkiProfile.ts
@@ -1,4 +1,5 @@
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
+
 import { HelsinkiUserProfileState, ProcessingStatus } from '../types';
 import { fetchHelsinkiProfileAction } from '../actions/helsinkiProfile';
 
@@ -6,22 +7,21 @@ const initialState: HelsinkiUserProfileState = {
   profile: {},
 } as HelsinkiUserProfileState;
 
-const helsinkiProfileReducer = reducerWithInitialState<HelsinkiUserProfileState>(
-  initialState
-)
-  .case(fetchHelsinkiProfileAction.started, state => ({
-    ...state,
-    fetchingStatus: ProcessingStatus.PROCESSING,
-  }))
-  .case(fetchHelsinkiProfileAction.done, (state, action) => ({
-    ...state,
-    fetchingStatus: ProcessingStatus.SUCCESS,
-    profile: action.result,
-  }))
-  .case(fetchHelsinkiProfileAction.failed, (state, action) => ({
-    ...state,
-    error: action.error,
-    fetchingStatus: ProcessingStatus.FAILURE,
-  }));
+const helsinkiProfileReducer =
+  reducerWithInitialState<HelsinkiUserProfileState>(initialState)
+    .case(fetchHelsinkiProfileAction.started, state => ({
+      ...state,
+      fetchingStatus: ProcessingStatus.PROCESSING,
+    }))
+    .case(fetchHelsinkiProfileAction.done, (state, action) => ({
+      ...state,
+      fetchingStatus: ProcessingStatus.SUCCESS,
+      profile: action.result,
+    }))
+    .case(fetchHelsinkiProfileAction.failed, (state, action) => ({
+      ...state,
+      error: action.error,
+      fetchingStatus: ProcessingStatus.FAILURE,
+    }));
 
 export default helsinkiProfileReducer;
