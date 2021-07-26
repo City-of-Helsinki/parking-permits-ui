a7aee3fe3e9b8c91abbbf292efdbb7c5cb2c9b77
Aman Yadav <amanpdyadav@gmail.com>
2021-07-26 12:22:55 +0300

    Move the providers to top level
    Refs PV-196

--- src/common/apiAccessTokenProvider.tsx
+++ src/common/apiAccessTokenProvider.tsx
@@ -3,9 +3,8 @@ import React, { FC } from 'react';
 import { useApiAccessTokens } from '../client/hooks';
 import { ApiAccessTokenActions } from '../client/types';
 
-export const ApiAccessTokenContext = React.createContext<ApiAccessTokenActions | null>(
-  null
-);
+export const ApiAccessTokenContext =
+  React.createContext<ApiAccessTokenActions | null>(null);
 
 export const ApiAccessTokenProvider: FC<unknown> = ({ children }) => {
   const actions = useApiAccessTokens();
