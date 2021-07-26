de8b7293304a9cabe25fa1fdd0033da1d5fea738
Aman Yadav <amanpdyadav@gmail.com>
2021-07-26 12:48:49 +0300

    Update Hero component
    Refs PV-196

--- src/common/hero/Hero.tsx
+++ src/common/hero/Hero.tsx
@@ -1,38 +1,25 @@
-import React, { CSSProperties, FC } from 'react';
+import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
-import { Container } from 'reactstrap';
+import React, { CSSProperties, FC } from 'react';
 
 import './hero.scss';
-import classNames from 'classnames';
-import Stepper from '../stepper/Stepper';
 
 interface Props {
   title: string;
-  currentStep: number;
   style?: CSSProperties;
   className?: string;
 }
 
-const Hero: FC<Props> = ({
-  title,
-  currentStep,
-  style,
-  className,
-}): React.ReactElement => {
+const Hero: FC<Props> = ({ title, style, className }): React.ReactElement => {
   const { t } = useTranslation();
 
   return (
     <div
-      className={classNames(`parking-permit-hero ${className}`, {
-        hideInMobile: currentStep > 1,
-      })}
+      className={classNames(`parking-permit-hero ${className}`)}
       style={{ ...style }}>
-      <Container>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <h1 className="parking-permit-hero__title">{t(title)}</h1>
-          <Stepper currentStep={currentStep} />
-        </div>
-      </Container>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div className="parking-permit-hero__title">{t(title)}</div>
+      </div>
     </div>
   );
 };
