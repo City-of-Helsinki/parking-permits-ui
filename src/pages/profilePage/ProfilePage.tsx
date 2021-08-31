054b8c6f49c8879f3e194a92d5337dce98c260b0
Aman Yadav <amanpdyadav@gmail.com>
2021-08-31 16:19:07 +0300

    Update profile page component

--- src/pages/profilePage/ProfilePage.tsx
+++ src/pages/profilePage/ProfilePage.tsx
@@ -3,34 +3,39 @@ import { useSelector } from 'react-redux';
 import { Card, Container } from 'reactstrap';
 import { useTranslation } from 'react-i18next';
 
-import { StoreState, UserAddress } from '../../redux';
+import { StoreState } from '../../redux';
 import { ApiAccessTokenActions } from '../../client/types';
 import { ApiAccessTokenContext } from '../../common/apiAccessTokenProvider';
 
 const T_PATH = 'pages.profilePage.ProfilePage';
 
 const ProfilePage = (): React.ReactElement => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   const actions = useContext(ApiAccessTokenContext) as ApiAccessTokenActions;
   const { helsinkiProfileState } = useSelector((state: StoreState) => state);
-  const { firstName, lastName, addresses } = helsinkiProfileState.profile;
+  const { firstName, lastName, primaryAddress, otherAddress } =
+    helsinkiProfileState.profile;
   const { getStatus: getApiAccessTokenStatus } = actions;
   const apiAccessTokenStatus = getApiAccessTokenStatus();
   if (apiAccessTokenStatus === 'error') {
     return <div>{t(`${T_PATH}.apiError`)}</div>;
   }
 
-  const getAddresses = (userAddresses: UserAddress[]) =>
-    userAddresses.map(address => (
+  const getAddresses = () =>
+    [primaryAddress, otherAddress].map((address, index) => (
       <div key={address.id}>
         <span
           style={{
             fontSize: 'var(--fontsize-heading-s)',
             marginTop: 'var(--spacing-s)',
           }}>
-          {address.primary ? 'Primary Address: ' : 'Other address: '}
+          {index === 0 ? 'Primary Address: ' : 'Other address: '}
         </span>
-        <span>{`${address.address}, ${address.postalCode}, ${address.city}`}</span>
+        <span>{`${
+          i18n.language === 'sv' ? address.streetNameSv : address.streetName
+        }, ${address.postalCode}, ${
+          i18n.language === 'sv' ? address.citySv : address.city
+        }`}</span>
       </div>
     ));
   return (
@@ -50,7 +55,7 @@ const ProfilePage = (): React.ReactElement => {
           </span>
           <span>{`${firstName} ${lastName}`}</span>
         </div>
-        {getAddresses(Object.values(addresses))}
+        {getAddresses()}
       </Card>
       <div style={{ fontSize: 'var(--fontsize-heading-m)' }}>Data:</div>
       <Card>
