7796b75a0ad0fc617ef9a1c9b4b3e63d8b10329a
Aman Yadav <amanpdyadav@gmail.com>
2021-09-14 10:51:54 +0300

    Remove caching from graphql client
    Due to caching while changing the primary type
    of vehicle it doesn't call the backend to get
    the new list of permits details as the params
    sent from frontend is always going to be same.
    
    Also, maintaining the state is handled by redux
    so in this case we dont need caching at graphql
    client.
    
    Refs PV-203

--- src/graphql/graphqlClient.ts
+++ src/graphql/graphqlClient.ts
@@ -1,5 +1,6 @@
 import {
   ApolloClient,
+  DefaultOptions,
   InMemoryCache,
   NormalizedCacheObject,
 } from '@apollo/client';
@@ -8,9 +9,20 @@ import { FetchError } from '../client/types';
 export type GraphQLClient = ApolloClient<NormalizedCacheObject>;
 export type GraphQLClientError = Pick<FetchError, 'error' | 'message'>;
 
+const defaultOptions: DefaultOptions = {
+  mutate: {
+    errorPolicy: 'all',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+};
+
 export function createGraphQLClient(uri: string, token: string): GraphQLClient {
   return new ApolloClient({
     uri,
+    defaultOptions,
     cache: new InMemoryCache(),
     headers: {
       Authorization: `Bearer ${token}`,
