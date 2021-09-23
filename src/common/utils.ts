f235c1b32efc1ab06a1bb0b7e666564754e43fa1
Aman Yadav <amanpdyadav@gmail.com>
2021-09-23 10:26:27 +0300

    Add street number to address formatter
    Refs PV-237

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -2,8 +2,9 @@ import { UserAddress } from '../redux/types';
 
 // eslint-disable-next-line import/prefer-default-export
 export const formatAddress = (address: UserAddress, lang: string): string => {
-  const { streetName, streetNameSv, city, citySv, postalCode } = address;
+  const { streetName, streetNameSv, city, citySv, postalCode, streetNumber } =
+    address;
   const addressStreet = lang === 'sv' ? streetNameSv : streetName;
   const addressCity = lang === 'sv' ? citySv : city;
-  return `${addressStreet}, ${postalCode} ${addressCity}`;
+  return `${addressStreet} ${streetNumber}, ${postalCode} ${addressCity}`;
 };
