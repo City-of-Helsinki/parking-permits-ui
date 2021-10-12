7d74c32957af51eb6ebd5d20d1b4d2dcafd90016
Mingfeng Yu <mfdevworld@gmail.com>
2021-10-12 17:21:48 +0300

    Refactor graphql clients api token verification
    Previously the api tokens obtained from tunnistamo are not
    verified at all and we generate our own token and passing it
    around between backend and frontend.
    
    With this change, we are now passing both parking-permits and
    helsinki profile  API tokens to backend. The parking-permits
    API token is passed through Authorization header, and the API
    token for Helsinki Profile is stored in a custom HTTP header
    X-Authorization.
    
    Refs: PV-247

--- src/hooks/utils.ts
+++ src/hooks/utils.ts
@@ -1,26 +1,27 @@
 import { getClient } from '../client/oidc-react';
 import { createGraphQLClient, GraphQLClient } from '../graphql/graphqlClient';
+import { getEnv } from '../utils';
 
-let profileGqlClient: GraphQLClient;
+let gqlClient: GraphQLClient;
 
-export function getProfileApiToken(): string | undefined {
-  const client = getClient();
-  const tokenKey = process.env.REACT_APP_PROFILE_AUDIENCE;
-  if (!tokenKey) {
-    return undefined;
-  }
-  const apiTokens = client.getApiTokens();
-  return apiTokens[tokenKey];
-}
-
-export function getProfileGqlClient(): GraphQLClient | undefined {
-  if (!profileGqlClient) {
-    const token = getProfileApiToken();
-    const uri = process.env.REACT_APP_PROFILE_BACKEND_URL;
-    if (!token || !uri) {
+// eslint-disable-next-line import/prefer-default-export
+export function getGqlClient(): GraphQLClient | undefined {
+  if (!gqlClient) {
+    const client = getClient();
+    const tokens = client.getApiTokens();
+    const parkingPermitsTokenKey = getEnv('REACT_APP_PARKING_PERMITS_AUDIENCE');
+    const profileTokenKey = getEnv('REACT_APP_PROFILE_AUDIENCE');
+    const parkingPermitsApiToken = tokens[parkingPermitsTokenKey];
+    const helsinkiProfileApiToken = tokens[profileTokenKey];
+    if (!parkingPermitsApiToken || !helsinkiProfileApiToken) {
       return undefined;
     }
-    profileGqlClient = createGraphQLClient(uri, token);
+    const uri = getEnv('REACT_APP_PROFILE_BACKEND_URL');
+    gqlClient = createGraphQLClient(
+      uri,
+      parkingPermitsApiToken,
+      helsinkiProfileApiToken
+    );
   }
-  return profileGqlClient;
+  return gqlClient;
 }
