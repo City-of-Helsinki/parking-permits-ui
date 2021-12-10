d776be7e52797c37899ef38cb944dddc4c3d1351
Aman Yadav <amanpdyadav@gmail.com>
2021-12-10 09:13:44 +0200

    Remove talpa order creation from frontend
    Refs PV-304

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,6 @@ export * from './actions';
 export * from './enums';
 export * from './graphql';
 export * from './permits';
-export * from './talpa';
 export * from './user';
 
 export const PARKING_PERMIT_TOKEN = 'parking_permit_token';
