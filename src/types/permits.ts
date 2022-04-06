9912027ba67cebe3b1b733006f62fe79918427db
Aman Yadav <amanpdyadav@gmail.com>
2022-04-06 10:42:58 +0300

    Remove subscriptionId from permit as its moved to Order
    Refs PV-313

--- src/types/permits.ts
+++ src/types/permits.ts
@@ -14,7 +14,6 @@ export interface Product {
 export type Permit = {
   id: string;
   orderId: string;
-  subscriptionId: string;
   startType?: ParkingStartType;
   startTime?: Date | string;
   endTime?: Date | string | null;
