74698132611019f9d82c1d07094232baf3d1c558
Aman Yadav <amanpdyadav@gmail.com>
2022-05-02 08:48:14 +0300

    Move vehicle detail parts from change vehicle to vehicleDetail component
    Refs PV-335

--- src/pages/changeVehicle/ChangeVehicle.tsx
+++ src/pages/changeVehicle/ChangeVehicle.tsx
@@ -1,163 +1,142 @@
-import {
-  Button,
-  Card,
-  IconArrowLeft,
-  IconArrowRight,
-  LoadingSpinner,
-  Notification,
-  TextInput,
-} from 'hds-react';
-import React, { useCallback, useContext, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { useContext, useState } from 'react';
 import { useParams } from 'react-router';
 import { Navigate, useNavigate } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
-import {
-  getVehicleInformation,
-  updatePermitVehicle,
-} from '../../graphql/permitGqlClient';
+import PriceChangePreview from '../../common/editPermits/PriceChangePreview';
+import Refund from '../../common/editPermits/Refund';
+import { getChangeTotal } from '../../common/editPermits/utils';
+import VehicleDetails from '../../common/editPermits/VehicleDetails';
+import { updatePermitVehicle } from '../../graphql/permitGqlClient';
 import { PermitStateContext } from '../../hooks/permitProvider';
-import { ROUTES, Vehicle } from '../../types';
-import { formatDate } from '../../utils';
-import './changeVehicle.scss';
+import { UserProfileContext } from '../../hooks/userProfileProvider';
+import { PermitPriceChanges, ROUTES, Vehicle } from '../../types';
 
-const T_PATH = 'pages.changeVehicle.ChangeVehicle';
+enum PriceChangeType {
+  HIGHER_PRICE = 2,
+  // eslint-disable-next-line no-magic-numbers
+  LOWER_PRICE = 0.5,
+  NO_CHANGE = 1,
+}
+
+enum ChangeVehicleStep {
+  VEHICLE,
+  PRICE_PREVIEW,
+  REFUND,
+}
 
 const ChangeVehicle = (): React.ReactElement => {
-  const { t } = useTranslation();
   const navigate = useNavigate();
   const params = useParams();
   const permitCtx = useContext(PermitStateContext);
-  const [tempRegistration, setTempRegistration] = useState('');
+  const profileCtx = useContext(UserProfileContext);
   const [vehicle, setVehicle] = useState<Vehicle>();
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
 
-  const fetchVehicleInformation = useCallback(async () => {
-    setLoading(true);
-    setError('');
-    await getVehicleInformation(tempRegistration)
-      .then(setVehicle)
-      .catch(errors =>
-        setError(errors?.map((e: { message: string }) => e?.message).join('\n'))
-      );
-    setLoading(false);
-  }, [tempRegistration]);
+  const [step, setStep] = useState<ChangeVehicleStep>(
+    ChangeVehicleStep.VEHICLE
+  );
+
+  const [priceChangesList, setPriceChangesList] = useState<
+    PermitPriceChanges[]
+  >([]);
 
   const permit = permitCtx?.getPermits().find(p => p.id === params.permitId);
   if (!permit) {
     return <Navigate to={ROUTES.VALID_PERMITS} />;
   }
-  const inputRegistration = (event: { target: { value: string } }) => {
-    setError('');
-    const { value } = event.target;
-    setTempRegistration(value);
-  };
 
   const getMultiplier = (): number => {
-    if (!vehicle) return 1;
+    if (!vehicle) return PriceChangeType.NO_CHANGE;
     const { isLowEmission: currentState } = permit.vehicle;
     const { isLowEmission: newState } = vehicle;
     if (currentState !== newState) {
-      // eslint-disable-next-line no-magic-numbers
-      return currentState && !newState ? 2 : 0.5;
+      return currentState && !newState
+        ? PriceChangeType.HIGHER_PRICE
+        : PriceChangeType.LOWER_PRICE;
     }
-    return 1;
+    return PriceChangeType.NO_CHANGE;
+  };
+
+  const updateAndNavigateToOrderView = async (accountNumber?: string) => {
+    const { orderId } = await updatePermitVehicle(
+      permit.id,
+      vehicle?.id,
+      accountNumber
+    );
+    await permitCtx?.fetchPermits();
+    navigate(`${ROUTES.SUCCESS}?orderId=${orderId}`);
   };
 
   const continueTo = async () => {
     const multiplier = getMultiplier();
-    if (multiplier === 1) {
-      await updatePermitVehicle(permit.id, vehicle?.id);
-      await permitCtx?.fetchPermits();
-      navigate(`${ROUTES.SUCCESS}?orderId=${permit?.orderId}`);
+    if (
+      step === ChangeVehicleStep.VEHICLE &&
+      multiplier === PriceChangeType.NO_CHANGE
+    ) {
+      await updateAndNavigateToOrderView();
+    }
+
+    if (
+      step === ChangeVehicleStep.VEHICLE &&
+      multiplier !== PriceChangeType.NO_CHANGE
+    ) {
+      setPriceChangesList([
+        {
+          permit,
+          priceChanges: permit.products.map(product => ({
+            product: permit.parkingZone.name,
+            previousPrice: product.unitPrice,
+            newPrice: product.unitPrice * multiplier,
+            priceChange: product.unitPrice * multiplier - product.unitPrice,
+            priceChangeVat: product.vat * multiplier,
+            startDate: product.startDate,
+            endDate: product.endDate,
+            monthCount: permit.monthsLeft,
+          })),
+        },
+      ]);
+
+      if (multiplier === PriceChangeType.HIGHER_PRICE) {
+        const { checkoutUrl } = await updatePermitVehicle(
+          permit.id,
+          vehicle?.id
+        );
+        await permitCtx?.fetchPermits();
+        if (checkoutUrl && profileCtx) {
+          const { id: userId } = profileCtx.getProfile();
+          window.open(`${checkoutUrl}?user=${userId}`, '_self');
+        }
+      } else {
+        setStep(ChangeVehicleStep.PRICE_PREVIEW);
+      }
     }
   };
 
   return (
     <div className="change-vehicle-component">
-      {error && (
-        <Notification type="error" className="error-notification">
-          {t(error || '')}
-        </Notification>
+      {step === ChangeVehicleStep.VEHICLE && (
+        <VehicleDetails
+          permit={permit}
+          vehicle={vehicle}
+          setVehicle={setVehicle}
+          priceChangeMultiplier={getMultiplier()}
+          onContinue={continueTo}
+        />
       )}
-      <div className="section-label">{t(`${T_PATH}.sectionLabel`)}</div>
-      <TextInput
-        id={uuidv4()}
-        maxLength={7}
-        value={tempRegistration}
-        label={t(`${T_PATH}.input.label`)}
-        onChange={inputRegistration}
-        className="registration-input"
-        helperText={t(`${T_PATH}.input.helpText`)}
-      />
-      <Button
-        variant="secondary"
-        className="change-btn"
-        disabled={loading}
-        onClick={fetchVehicleInformation}>
-        {!loading && t(`${T_PATH}.changeBtn`)}
-        {loading && <LoadingSpinner small />}
-      </Button>
-
-      {vehicle && (
-        <Card className="card">
-          <div className="car-details">
-            <div className="registration-number">
-              {vehicle.registrationNumber}
-            </div>
-            <div className="car-model">
-              {vehicle.manufacturer} {vehicle.model}
-            </div>
-            <div
-              className={`emission-level ${
-                vehicle.isLowEmission ? 'low-emission' : 'high-emission'
-              }`}>
-              {t(
-                `${T_PATH}.${
-                  vehicle.isLowEmission
-                    ? 'lowEmissionVehicle'
-                    : 'highEmissionVehicle'
-                }`
-              )}
-            </div>
-            {permit &&
-              vehicle &&
-              permit.products.map(product => (
-                <div key={uuidv4()} className="price">
-                  {permit.vehicle.isLowEmission !== vehicle.isLowEmission && (
-                    <div className="original invalid">{`${product.unitPrice} €/KK`}</div>
-                  )}
-                  <div className="offer">{`${
-                    product.unitPrice * getMultiplier()
-                  } €/KK`}</div>
-                  <div>{`(${formatDate(product.startDate)} - ${formatDate(
-                    product.endDate
-                  )})`}</div>
-                </div>
-              ))}
-          </div>
-        </Card>
+      {step === ChangeVehicleStep.PRICE_PREVIEW && priceChangesList && (
+        <PriceChangePreview
+          className="price-change-preview"
+          priceChangesList={priceChangesList}
+          onCancel={() => setStep(ChangeVehicleStep.VEHICLE)}
+          onConfirm={() => setStep(ChangeVehicleStep.REFUND)}
+        />
       )}
-      <div className="action-buttons">
-        <Button
-          theme="black"
-          className="action-btn"
-          disabled={!vehicle}
-          iconRight={<IconArrowRight />}
-          onClick={() => continueTo()}>
-          {t(`${T_PATH}.actionBtn.continue`)}
-        </Button>
-
-        <Button
-          className="action-btn"
-          theme="black"
-          variant="secondary"
-          iconLeft={<IconArrowLeft />}
-          onClick={() => navigate(ROUTES.VALID_PERMITS)}>
-          {t(`${T_PATH}.actionBtn.back`)}
-        </Button>
-      </div>
+      {step === ChangeVehicleStep.REFUND && (
+        <Refund
+          refundTotal={-getChangeTotal(priceChangesList, 'priceChange')}
+          refundTotalVat={-getChangeTotal(priceChangesList, 'priceChangeVat')}
+          onCancel={() => setStep(ChangeVehicleStep.PRICE_PREVIEW)}
+          onConfirm={updateAndNavigateToOrderView}
+        />
+      )}
     </div>
   );
 };
