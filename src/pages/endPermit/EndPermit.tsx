411f82c46df41ed6dca744dc91afb870869bcb8e
Aman Yadav <amanpdyadav@gmail.com>
2022-05-08 19:56:18 +0300

    Use same Refund and Price preview components
    Use same Refund and Price preview components for ending the permits
    as well.
    
    Refs PV-275

--- src/pages/endPermit/EndPermit.tsx
+++ src/pages/endPermit/EndPermit.tsx
@@ -1,18 +1,23 @@
+import { addMonths } from 'date-fns';
 import queryString from 'query-string';
 import React, { useContext, useState } from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
-import AccountDetail from '../../common/accountDetail/AccountDetail';
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+import PriceChangePreview from '../../common/editPermits/PriceChangePreview';
+import Refund from '../../common/editPermits/Refund';
+import { getChangeTotal } from '../../common/editPermits/utils';
 import EndPermitResult from '../../common/endPermitResult/EndPermitResult';
-import Refund from '../../common/refund/Refund';
 import { PermitStateContext } from '../../hooks/permitProvider';
 import { UserProfileContext } from '../../hooks/userProfileProvider';
-import { EndPermitStep, PermitEndType, ROUTES, UserProfile } from '../../types';
+import { EndPermitStep, ROUTES, UserProfile } from '../../types';
 import './endPermit.scss';
 
 const EndPermit = (): React.ReactElement => {
   const { search } = useLocation();
+  const navigate = useNavigate();
   const profileCtx = useContext(UserProfileContext);
-  const [endPermitState, setEndPermitState] = useState(EndPermitStep.REFUND);
+  const [endPermitState, setEndPermitState] = useState(
+    EndPermitStep.PRICE_PREVIEW
+  );
 
   const permitCtx = useContext(PermitStateContext);
 
@@ -26,30 +31,50 @@ const EndPermit = (): React.ReactElement => {
   const permitIds = typeof ids === 'string' ? [ids] : ids;
   const permits = validPermits.filter(p => permitIds?.indexOf(p.id) !== -1);
   const getsRefund = permits.some(p => p.monthsLeft || 0);
-  const setState = (state: EndPermitStep) => setEndPermitState(state);
+
+  const priceChangesList = permits.map(permit => ({
+    vehicle: permit.vehicle,
+    priceChanges: permit.products.map(product => ({
+      product: product.name,
+      previousPrice: product.unitPrice,
+      newPrice: product.unitPrice,
+      priceChange: product.unitPrice,
+      priceChangeVat: product.vat,
+      startDate: addMonths(
+        new Date(product.startDate),
+        permit.monthsLeft
+      ).toString(),
+      endDate: product.endDate,
+      monthCount: permit.monthsLeft,
+    })),
+  }));
+
   return (
     <div className="end-permit-component">
       <div style={{ marginTop: 'var(--spacing-l)' }} />
-      {endPermitState === EndPermitStep.REFUND &&
-        permitCtx?.endValidPermits && (
-          <Refund
-            permits={permits}
-            endType={endType as PermitEndType}
-            profile={profile}
-            getsRefund={getsRefund}
-            endValidPermits={permitCtx.endValidPermits}
-            setEndPermitState={setState}
-          />
-        )}
-      {endPermitState === EndPermitStep.ACCOUNT &&
-        permitCtx?.endValidPermits && (
-          <AccountDetail
-            permits={permits}
-            endType={endType as PermitEndType}
-            setEndPermitState={setState}
-            endValidPermits={permitCtx.endValidPermits}
-          />
-        )}
+      {endPermitState === EndPermitStep.REFUND && permitCtx?.endValidPermits && (
+        <Refund
+          refundTotal={-getChangeTotal(priceChangesList, 'priceChange')}
+          refundTotalVat={-getChangeTotal(priceChangesList, 'priceChangeVat')}
+          onCancel={() => setEndPermitState(EndPermitStep.PRICE_PREVIEW)}
+          onConfirm={accountNumber => {
+            permitCtx?.endValidPermits(
+              permitIds as string[],
+              endType as string,
+              accountNumber
+            );
+            setEndPermitState(EndPermitStep.RESULT);
+          }}
+        />
+      )}
+      {endPermitState === EndPermitStep.PRICE_PREVIEW && priceChangesList && (
+        <PriceChangePreview
+          className="price-change-preview"
+          priceChangesList={priceChangesList}
+          onCancel={() => navigate(ROUTES.VALID_PERMITS)}
+          onConfirm={() => setEndPermitState(EndPermitStep.REFUND)}
+        />
+      )}
       {endPermitState === EndPermitStep.RESULT && (
         <EndPermitResult getsRefund={getsRefund} email={profile.email} />
       )}
