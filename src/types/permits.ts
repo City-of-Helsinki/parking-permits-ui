8a61f8b4cd513f8b5e8b91c1859c7f9c30b39a88
Aman Yadav <amanpdyadav@gmail.com>
2022-06-22 09:09:58 +0300

    Add functionality for showing receipt URL
    Refs PV-435

--- src/types/permits.ts
+++ src/types/permits.ts
@@ -15,6 +15,7 @@ export interface Product {
 export type Permit = {
   id: string;
   talpaOrderId: string;
+  receiptUrl: string;
   startType?: ParkingStartType;
   startTime?: Date | string;
   endTime?: Date | string | null;
