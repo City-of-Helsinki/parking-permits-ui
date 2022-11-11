7f039e6307939e063907a6692d3c525a59e1d65e
Aman Yadav <amanpdyadav@gmail.com>
2022-11-11 12:04:34 +0200

    Fix the incorrect refund calcuations while ending the permit
    Refs PV-487

--- src/pages/endPermit/EndPermit.tsx
+++ src/pages/endPermit/EndPermit.tsx
@@ -1,4 +1,4 @@
-import { addMonths } from 'date-fns';
+import { intervalToDuration } from 'date-fns';
 import queryString from 'query-string';
 import React, { useContext, useState } from 'react';
 import { Navigate, useLocation, useNavigate } from 'react-router-dom';
@@ -8,8 +8,39 @@ import { getChangeTotal } from '../../common/editPermits/utils';
 import EndPermitResult from '../../common/endPermitResult/EndPermitResult';
 import { PermitStateContext } from '../../hooks/permitProvider';
 import { UserProfileContext } from '../../hooks/userProfileProvider';
-import { EndPermitStep, ROUTES, UserProfile } from '../../types';
+import {
+  EndPermitStep,
+  PermitEndType,
+  Product,
+  ROUTES,
+  UserProfile,
+} from '../../types';
 import './endPermit.scss';
+
+const dateAsNumber = (date: Date | string): number => new Date(date).valueOf();
+
+const getMonthCount = (
+  permitEndStartDate: Date,
+  permitStartTime: string,
+  product: Product
+) => {
+  // It should consider the start time of the permit.
+  // Eg: If permit was bought and started just before the permitEndStartDate
+  // then it should be counted as a full month used and if the start date is in
+  // the future. It should refund the whole month.
+  if (
+    dateAsNumber(permitStartTime) > permitEndStartDate.valueOf() ||
+    dateAsNumber(product.startDate) > permitEndStartDate.valueOf()
+  ) {
+    return product.quantity;
+  }
+
+  const intervalDuration = intervalToDuration({
+    start: new Date(),
+    end: new Date(product.endDate),
+  });
+  return intervalDuration.months || 0;
+};
 
 const EndPermit = (): React.ReactElement => {
   const { search } = useLocation();
@@ -32,22 +63,35 @@ const EndPermit = (): React.ReactElement => {
   const permits = validPermits.filter(p => permitIds?.indexOf(p.id) !== -1);
   const getsRefund = permits.some(p => p.monthsLeft || 0);
 
-  const priceChangesList = permits.map(permit => ({
-    vehicle: permit.vehicle,
-    priceChanges: permit.products.map(product => ({
-      product: product.name,
-      previousPrice: product.unitPrice,
-      newPrice: product.unitPrice,
-      priceChange: product.unitPrice,
-      priceChangeVat: product.vat,
-      startDate: addMonths(
-        new Date(product.startDate),
-        permit.monthsLeft
-      ).toString(),
-      endDate: product.endDate,
-      monthCount: permit.monthsLeft,
-    })),
-  }));
+  const priceChangesList = permits.map(permit => {
+    // User can end the permit today of from the end of current period.
+    const endingOfPermitStartDate =
+      endType === PermitEndType.AFTER_CURRENT_PERIOD
+        ? new Date(permit.currentPeriodEndTime)
+        : new Date();
+    return {
+      vehicle: permit.vehicle,
+      priceChanges: permit.products
+        .filter(
+          product =>
+            endingOfPermitStartDate.valueOf() <= dateAsNumber(product.endDate)
+        )
+        .map(product => ({
+          product: product.name,
+          previousPrice: product.unitPrice,
+          newPrice: product.unitPrice,
+          priceChange: product.unitPrice,
+          priceChangeVat: product.vat * product.unitPrice,
+          startDate: product.startDate,
+          endDate: product.endDate,
+          monthCount: getMonthCount(
+            endingOfPermitStartDate,
+            permit.startTime as string,
+            product
+          ),
+        })),
+    };
+  });
 
   return (
     <div className="end-permit-component">
