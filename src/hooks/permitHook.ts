a092afe42ca21de3d319d1fc342239fe18a69526
Vesa-Matti Virolainen <vesa-matti.virolainen@anders.com>
2025-10-10 10:54:05 +0300

    feat: implement permitsHaveOutdatedAddresses
    implement permitsHaveOutdatedAddresses utility-function in permitHook,
    adjust PermitActions-type
    
    refs PV-924

--- src/hooks/permitHook.ts
+++ src/hooks/permitHook.ts
@@ -125,6 +125,17 @@ const usePermitState = (): PermitActions => {
     }
   }, [fetchPermits, status, profile]);
 
+  const getValidPermits = () =>
+    permits.filter(
+      permit =>
+        [PermitStatus.VALID, PermitStatus.PAYMENT_IN_PROGRESS].includes(
+          permit.status
+        ) ||
+        ((permit.status === PermitStatus.DRAFT ||
+          permit.status === PermitStatus.PRELIMINARY) &&
+          permit.isOrderConfirmed)
+    );
+
   return {
     getStatus: () => status,
     getStep: () => step,
@@ -144,18 +155,15 @@ const usePermitState = (): PermitActions => {
             permit.status === PermitStatus.PRELIMINARY
         )
         .sort(a => (a.primaryVehicle ? -1 : 1)),
-    getValidPermits: () =>
-      permits.filter(
-        permit =>
-          [PermitStatus.VALID, PermitStatus.PAYMENT_IN_PROGRESS].includes(
-            permit.status
-          ) ||
-          ((permit.status === PermitStatus.DRAFT ||
-            permit.status === PermitStatus.PRELIMINARY) &&
-            permit.isOrderConfirmed)
-      ),
+    getValidPermits: () => getValidPermits(),
     getChangeAddressPriceChanges,
     changeAddress: (addressId, iban) => changeAddressRequest(addressId, iban),
+    // For all permits, check if the permit has address changed-flag
+    // on. (Which in turn is based on comparison to the users'
+    // addresses.) As the permits _should_ have the same address, a
+    // single outdated address in enough.
+    permitsHaveOutdatedAddresses: () =>
+      getValidPermits().some(p => p.addressChanged),
     setStep: count => setStep(count),
     updatePermit: (payload, permitId) => updatePermit(payload, permitId),
     deletePermit: permitId => deletePermit(permitId),
